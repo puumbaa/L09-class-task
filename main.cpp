e9321058fc52c0ebbb305c83c4f3b61659306fa7
Elnur Sardarov <sardarovelnur@gmail.com>
2021-05-05 13:19:13 +0300

    binary_search

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
+bool binary_search(int key, int *arr, int size) {
+    if (size <= 0) return false;
+
+    int middle = size / 2;
+    if (key > arr[middle])      return binary_search(key, arr + middle + 1, size - middle - 1);
+    else if (key < arr[middle]) return binary_search(key, arr, middle);
+
+    return true;
+}
+
+
 int main(int argc, char **argv) {
-  std::cout << "Hello, stranger!" << std::endl;
-  return 0;
+    int arr[] = {1, 3, 5, 6, 8, 10, 12, 21};
+    std::cout << binary_search(6, arr, 8);
+    return 0;
 }
